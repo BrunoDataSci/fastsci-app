a258f591a593a0f3884067880332ad785d4cad8f
DataSciBruno <brunogrupo@gmail.com>
2023-11-04 13:36:27 -0300

    make it better

--- main.py
+++ main.py
@@ -21,13 +21,6 @@ def main_page():
         global end_date
         global downloaded_dict
 
-        drawdown = None
-        image_base64 = None
-        symbol = None
-        start_date = None
-        end_date = None
-        downloaded_dict = {}
-
 
         symbol = request.form['symbol']
         start_date = request.form['start_date']
